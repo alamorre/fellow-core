3753ffdc40a54cdfe0496b21ed4ad1b418df92c9
Adam La Morre <alamorre@gmail.com>
2019-09-03 10:17:30 -0400

    new games are created

---
+++ games/serializers.py
@@ -0,0 +1,24 @@
+from rest_framework import serializers
+from games.models import Game, Block
+
+
+class BlockSerializer(serializers.ModelSerializer):
+    """
+    Serializer for the Block model
+    """
+    class Meta(object):
+        model = Block
+        fields = '__all__'
+
+
+class GameSerializer(serializers.ModelSerializer):
+    """
+    Serializer for the Game model and related blocks
+    """
+    id = serializers.ReadOnlyField
+    state_key = serializers.ReadOnlyField
+    blocks = BlockSerializer(many=True)
+
+    class Meta(object):
+        model = Game
+        fields = '__all__'
--- games/views.py
+++ games/views.py
@@ -3,6 +3,9 @@ from rest_framework.views import APIView
 from rest_framework.response import Response
 from rest_framework import status, permissions
 
+from games.models import Game, Block
+from games.serializers import GameSerializer
+
 
 class Games(APIView):
     permission_classes = (permissions.AllowAny,)
@@ -13,4 +16,15 @@ class Games(APIView):
         :param request: POST
         :return: return 201 for a new game
         """
-        return Response({"message": "New game buddy!"}, status=status.HTTP_200_OK)
+        # Start a new game
+        game = Game.objects.create()
+
+        # For 0 to 99, create a block for this game with i as index
+        for i in range(0, 100):
+            block = Block.objects.create(game=game, index=i)
+            print(str(block.game.pk))
+            print(str(block.index))
+
+        # Return the new data in a GameSerializer
+        serializer = GameSerializer(game, many=False)
+        return Response(serializer.data, status=status.HTTP_200_OK)
