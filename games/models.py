62f1b2c7d6b63614b596bd7d7a5f00d73f130e7f
Adam La Morre <alamorre@gmail.com>
2019-09-03 09:56:57 -0400

    ready to generate a new game script

--- games/models.py
+++ games/models.py
@@ -1,3 +1,18 @@
 from django.db import models
 
-# Create your models here.
+
+class Game(models.Model):
+    state_key = models.CharField(blank=True, null=True, max_length=1000)
+
+
+class Block(models.Model):
+    # Every block is associated to a game
+    game = models.ForeignKey(Game, related_name='blocks', on_delete=models.CASCADE)
+
+    # Where is the block (position 0 to 99)
+    index = models.PositiveIntegerField()
+
+    # Is the block a mine, flipped or flagged
+    is_mine = models.BooleanField(default=False)
+    is_flipped = models.BooleanField(default=False)
+    is_flagged = models.BooleanField(default=False)
