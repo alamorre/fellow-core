0fa5b6b166c9cebcf979bec7c17e2fc32a6d3240
Adam La Morre <alamorre@gmail.com>
2019-09-03 20:22:57 -0400

    sweep with more robust code

--- games/sweeper.py
+++ games/sweeper.py
@@ -4,48 +4,64 @@ from games.models import Block
 class BlockQueue:
     def __init__(self):
         self.items = []
+        self.visited = set()
 
     def is_empty(self):
         return self.items == []
 
-    def enqueue(self, item):
-        self.items.insert(0, item)
+    def enqueue_unique(self, item):
+        """
+        This function checks and adds only unique Blocks in O(1)
+        :param item:
+        :return:
+        """
+        if item not in self.visited:
+            self.items.insert(0, item)
+            self.visited.add(item)
 
     def dequeue(self):
         return self.items.pop()
 
 
+# Get the top left neighbour block if it exists
 def get_top_left(block):
     return Block.objects.get(game=block.game,
                              index=block.index - 11) if block.index > 9 and block.index % 10 is not 0 else None
 
 
+# Get the top center neighbour block if it exists
 def get_top(block):
     return Block.objects.get(game=block.game, index=block.index - 10) if block.index > 9 else None
 
 
+# Get the top right neighbour block if it exists
 def get_top_right(block):
     return Block.objects.get(game=block.game,
                              index=block.index - 9) if block.index > 9 and block.index % 10 is not 9 else None
 
 
+# Get the left neighbour block if it exists
 def get_left(block):
     return Block.objects.get(game=block.game, index=block.index - 1) if block.index % 10 is not 0 else None
 
 
+# Get the Right neighbour block if it exists
 def get_right(block):
     return Block.objects.get(game=block.game, index=block.index + 1) if block.index % 10 is not 9 else None
 
 
+# Get the bottom left neighbour block if it exists
 def get_bottom_left(block):
     return Block.objects.get(game=block.game,
                              index=block.index + 9) if block.index < 90 and block.index % 10 is not 0 else None
 
 
+# Get the bottom center neighbour block if it exists
 def get_bottom(block):
     return Block.objects.get(game=block.game, index=block.index + 10) if block.index < 90 else None
 
 
+# Get the bottom right neighbour block if it exists
 def get_bottom_right(block):
     return Block.objects.get(game=block.game,
                              index=block.index + 11) if block.index < 90 and block.index % 10 is not 9 else None
@@ -102,13 +118,11 @@ class Sweeper:
         """
         # Put the first block into the queue
         q = BlockQueue()
-        visited = BlockQueue()
-        q.enqueue(first_block)
+        q.enqueue_unique(first_block)
 
         # Keep filling the queue with 0 blocks
         while not q.is_empty():
             block = q.dequeue()
-            visited.enqueue(block)
             print(str(block))
 
             # Check if the block has no nearby mines
@@ -116,57 +130,43 @@ class Sweeper:
                 block.is_flipped = True
                 block.save()
 
-                # Try flipping then adding the top block to queue
+                # Flip then try enqueueing the top block
                 temp = get_top(block)
                 if temp is not None:
-                    if check_no_mines(temp) and temp not in visited.items:
-                        q.enqueue(temp)
+                    if check_no_mines(temp):
+                        q.enqueue_unique(temp)
                     temp.is_flipped = True
                     temp.save()
 
-                # Try flipping then adding the bottom block to queue
+                # Flip then try enqueueing the bottom block
                 temp = get_bottom(block)
                 if temp is not None:
-                    if check_no_mines(temp) and temp not in visited.items:
-                        q.enqueue(temp)
+                    if check_no_mines(temp):
+                        q.enqueue_unique(temp)
                     temp.is_flipped = True
                     temp.save()
 
-                # Try flipping then adding the left block to queue
+                # Flip then try enqueueing the left block
                 temp = get_left(block)
                 if temp is not None:
-                    if check_no_mines(temp) and temp not in visited.items:
-                        q.enqueue(temp)
+                    if check_no_mines(temp):
+                        q.enqueue_unique(temp)
                     temp.is_flipped = True
                     temp.save()
 
-                # Try flipping then adding the right block to queue
+                # Flip then try enqueueing the right block
                 temp = get_right(block)
                 if temp is not None:
-                    if check_no_mines(temp) and temp not in visited.items:
-                        q.enqueue(temp)
+                    if check_no_mines(temp):
+                        q.enqueue_unique(temp)
                     temp.is_flipped = True
                     temp.save()
 
                 # Simply flip the corners
-                temp = get_top_left(block)
-                if temp is not None:
-                    temp.is_flipped = True
-                    temp.save()
+                for c in [get_top_left(block), get_top_right(block), get_bottom_left(block), get_bottom_right(block)]:
+                    if c is not None:
+                        c.is_flipped = True
+                        c.save()
 
-                temp = get_top_right(block)
-                if temp is not None:
-                    temp.is_flipped = True
-                    temp.save()
-
-                temp = get_bottom_left(block)
-                if temp is not None:
-                    temp.is_flipped = True
-                    temp.save()
-
-                temp = get_bottom_right(block)
-                if temp is not None:
-                    temp.is_flipped = True
-                    temp.save()
 
 sweeper = Sweeper()
