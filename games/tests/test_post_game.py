402f2a327978b0e8c3d1a2d7bf420c0c794edd62
Adam La Morre <alamorre@gmail.com>
2019-09-04 20:48:51 -0400

    ready to test flip and flag blocks

---
+++ games/tests/test_post_game.py
@@ -0,0 +1,54 @@
+from rest_framework.test import APITestCase, APIRequestFactory
+
+from games.views import Games
+from games.models import Game, Block
+
+from games.constants import NUMBER_OF_MINES, NUMBER_OF_BLOCKS
+
+
+class GamePostTestCase(APITestCase):
+    """
+    This Test Case is for Posting a new Game
+    """
+
+    def setUp(self):
+        pass
+
+    def tearDown(self):
+        """
+        For tear down, delete all games
+        """
+        Game.objects.all().delete()
+
+    def test_post_new_deal_makes_correct_blocks_and_mines(self):
+        # Assert there are no games and blocks
+        self.assertEqual(len(Game.objects.all()), 0)
+        self.assertEqual(len(Block.objects.all()), 0)
+
+        # Post a new Game
+        url = '/games/'
+        factory = APIRequestFactory()
+        view = Games.as_view()
+        request = factory.post(url, content_type='application/json')
+        response = view(request)
+
+        # Assert the response was okay
+        self.assertEqual(response.status_code, 200)
+
+        # Assert a new game with NUMBER_OF_BLOCKS blocks was made
+        self.assertEqual(len(Game.objects.all()), 1)
+        self.assertEqual(len(Block.objects.all()), NUMBER_OF_BLOCKS)
+
+        # Assert there are NUMBER_OF_MINES mines in the game
+        game = Game.objects.first()
+        self.assertEqual(len(Block.objects.filter(game=game, is_mine=True)), NUMBER_OF_MINES)
+
+        # Assert the blocks are PRIVATE (i.e. no mine data)
+        first_block = response.data.get('blocks')[0]
+        self.assertFalse(hasattr(first_block, 'is_mine'))
+
+        # Assert the game is not over
+        self.assertFalse(response.data.get('is_test'))
+        self.assertFalse(response.data.get('has_won'))
+        self.assertFalse(response.data.get('has_lost'))
+        self.assertEqual(response.data.get('flags_left'), NUMBER_OF_MINES)
